19fff6fc4ccb1bb89593e71138f0907014e43d0e
Keonlee Kim <kk545200@nate.com>
2024-12-31 23:33:34 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 108 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException{
+    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for(int i = 1; i <= N ; i++){
+            for(int j = 1; j<= i; j++){
+            sb.append('*');
+            }
+            sb.append('\n');
+        }
+        
+        System.out.print(sb);
+        br.close();
+    }
+}
