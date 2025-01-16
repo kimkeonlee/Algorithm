df4a5989ce092dda097123a2fb6236fa39b1f7c6
Keonlee Kim <kk545200@nate.com>
2025-01-16 21:51:10 +0900

    [Bronze II] Title: 단어의 개수, Time: 280 ms, Memory: 52800 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine().trim();
+        
+        if(input.isEmpty()){
+            System.out.println(0);
+        }else{
+            String[] words = input.split(" ");
+            System.out.println(words.length);
+        }
+        
+    }
+}
